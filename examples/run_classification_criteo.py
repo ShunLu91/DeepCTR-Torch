3a26f5be8aaa3b014fe4442c69d03b577507deed
lushun_mac <470651748@qq.com>
2020-03-19 14:58:05 +0800

    print(columns)

--- examples/run_classification_criteo.py
+++ examples/run_classification_criteo.py
@@ -9,8 +9,12 @@ from deepctr_torch.inputs import SparseFeat, DenseFeat, get_feature_names
 from deepctr_torch.models import *
 
 if __name__ == "__main__":
-    # data = pd.read_csv('/home/work/dataset/criteo/train.txt')
-    data = pd.read_csv('./criteo_sample.txt')
+    data = pd.read_csv('/home/work/dataset/criteo/train.txt')
+    # data = pd.read_csv('./criteo_sample.txt')
+    print(data.columns)
+
+    import sys
+    sys.exit(1)
 
     sparse_features = ['C' + str(i) for i in range(1, 27)]
     dense_features = ['I' + str(i) for i in range(1, 14)]
